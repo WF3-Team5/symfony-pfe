89daed55d822a28a3c7ac19e8a23097e0326cf60
SarahBen1 <sarah.benyoub@live.fr>
2019-02-18 16:51:24 +0100

    Formulaire inscription patient

--- src/Entity/Departement.php
+++ src/Entity/Departement.php
@@ -16,8 +16,42 @@ class Departement
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
     }
 }
