b7f142fe275572695172f85d8ddf3a68204d685f
SvetlanaNew <kikcecka@hotmail.com>
2019-02-22 10:19:08 +0100

    mise à jour

--- src/Controller/EspaceAssistantController.php
+++ src/Controller/EspaceAssistantController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Antecedents;
+use App\Entity\User;
 use App\Form\AntecedentsType;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,6 +33,29 @@ class EspaceAssistantController extends AbstractController
             'espace_assistant/index.html.twig',
             [
                 'form' => $form->createView(),
+            ]);
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/formulaire_recherche")
+     */
+    public function recherchePatient(User $user)
+    {
+        $em  = $this->getDoctrine()->getManager();
+        $user = New User();
+        $user = $em->findBy(
+            ['last_name' =>$lastName]
+        );
+
+        $form =$this->createForm(UserType::class, $user);
+        $form->handleRequest();
+
+        return $this->render(
+            'formulaire_recherche.html.twig',
+            [
+                'form' => $form->createView(),
+                'last_name' => $lastName
             ]);
     }
 
